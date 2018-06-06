17390ba01b3440b0dd383b4837a1e293108fc98d
Poine <poinix@gmail.com>
2018-06-06 15:12:38 +0200

    pvtol

--- docs/dql.md
+++ docs/dql.md
@@ -6,13 +6,53 @@ layout: default
 
 Qlearning is a reinforcement learning method suitable for problems with discrete state space and input (action) space problems. It has been extended to problemswith continuous state space () and then to problems with continuous input state ().
 
-###Q learning:
+### Q learning:
 
 `Q-Learning attempts to learn the value of being in a given state, and taking a specific action there.`
 
 time difference learning - Bellman Equation
 
 policy gradient
+
+
+### Simulations
+
+I started from an [implementation](https://github.com/pemami4911/deep-rl/tree/master/ddpg) of  *Deep Deterministic Policy Gradient* (DDPG) by Patrick Emami which had been tested on [OpenAi](https://openai.com/) [Gym](https://gym.openai.com/) [Pendulum](https://gym.openai.com/envs/Pendulum-v0/)
+
+#### Pendulum
+the code for the pendulum is [here](https://github.com/openai/gym/blob/master/gym/envs/classic_control/pendulum.py)
+
+The dymaics part reads:
+```python
+u = np.clip(u, -self.max_torque, self.max_torque)[0]
+newthdot = thdot + (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
+newth = th + newthdot*dt
+newthdot = np.clip(newthdot, -self.max_speed, self.max_speed)
+```
+It looks like
+
+$$
+\ddot{\theta} = -\frac{3g}{2l} \sin{(\theta+\pi)} + \frac{3}{ml^2} u
+$$
+
+mixed with a first order integration
+
+The cost part reads
+```python
+costs = angle_normalize(th)**2 + .1*thdot**2 + .001*(u**2)
+```
+
+
+and the observation reads:
+```python
+def _get_obs(self):
+	theta, thetadot = self.state
+	return np.array([np.cos(theta), np.sin(theta), thetadot])
+```
+
+
+`/dql__gym_pendulum.py --env Pendulum-v0 --render-env --train --summary-dir results/run_nonorm`
+
 
 ## References
 
--- src/test_gym_pvtol_pole.py
+++ src/test_gym_pvtol_pole.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, time, logging, numpy as np, matplotlib.pyplot as plt
+import sys, os, time, logging, numpy as np, matplotlib.pyplot as plt
 import control
 import gym, gym_foo.envs.pvtol_pole as pvtp, utils as ut
 LOG = logging.getLogger('test_gym_pvtol_pole')
@@ -11,12 +11,20 @@ LOG = logging.getLogger('test_gym_pvtol_pole')
 '''
 
 class StateFeedbackReg:
-    def __init__(self, env):
+    def __init__(self, env, _kind='place'):
         self.Ue = env.pvtp.Ue
         A, B = env.pvtp.jac()
-        poles=[-5, -5, -5, -5, -5, -5, -5, -5]
-        self.K = control.matlab.place(A, B, poles)
-        
+        if _kind == 'place':
+            poles=[-5, -5, -5, -5, -5, -5, -5, -5]
+            self.K = control.matlab.place(A, B, poles)
+        else:
+            Q = np.diag([1., 10., 1.5, 0.25, 0.01, 0.05, 0.005, 0.001])
+            R = np.diag([0.5, 0.5])
+            (self.K, X, E) = control.matlab.lqr(A, B, Q, R)
+        poles, vect_p = np.linalg.eig(A-np.dot(B, self.K))
+        LOG.info('gain:\n{}'.format(self.K))
+        LOG.info('poles:\n{}'.format(poles))
+            
     def predict(self, state, Xsp):
         dX = state - Xsp
         return (self.Ue - np.dot(self.K, dX)).squeeze()
@@ -24,9 +32,8 @@ class StateFeedbackReg:
 def sim(env, r):
     _dt, _len = 0.01, 1200
     time = np.linspace(0., _len*_dt, _len)
-    Xsp = np.zeros((_len, env.pvtp.s_size))
+    X, U, Xsp = np.zeros((_len, env.pvtp.s_size)), np.zeros((_len, env.pvtp.i_size)), np.zeros((_len, env.pvtp.s_size))
     Xsp[:,pvtp.PVTP.s_x] = ut.step_vec(time, dt=6, t0=2.7)
-    X, U = np.zeros((_len, env.pvtp.s_size)), np.zeros((_len, env.pvtp.i_size))
     X0 = np.array([1, 0, 0, 0, 0, 0, 0, 0])
     X[0] = env.reset(X0)[:env.pvtp.s_size]
     for i in range(1, len(time)):
@@ -36,7 +43,9 @@ def sim(env, r):
     U[-1] = U[-2]
     return time, X, U
     
-def plot(time, X, U):
+def plot(time, X, U, figure=None, filename=None):
+    if figure is None:
+        figure = ut.prepare_fig(figsize=(20.48, 7.68), margins=(0.04, 0.04, 0.98, 0.96, 0.13, 0.25))
     ax = plt.subplot(3, 2, 1)
     plt.plot(time, X[:, pvtp.PVTP.s_x])
     ut.decorate(ax, title='$x$', ylab='m')
@@ -53,14 +62,19 @@ def plot(time, X, U):
     plt.plot(time, U[:, pvtp.PVTP.i_f1])
     plt.plot(time, U[:, pvtp.PVTP.i_f2])
     ut.decorate(ax, title='$f$', ylab='N')
-
+    ut.save_if(filename)
+    return figure
+    
 import pyglet
 def render(env, time, X, U, fps=25, save=True):
+    dt_frame, i_frame = 1./fps, 0
     for i in range(len(time)):
-        env.pvtp.state = X[i]
-        env.render(info='t={:.1f}s'.format(time[i]))
-        if save:
-            pyglet.image.get_buffer_manager().get_color_buffer().save('/tmp/sc/screenshot_{:04d}.png'.format(i))
+        if time[i]>=i_frame*dt_frame:
+            env.pvtp.state = X[i]
+            env.render(info='t={:.1f}s'.format(time[i]))
+            if save:
+                pyglet.image.get_buffer_manager().get_color_buffer().save('/tmp/sc/screenshot_{:04d}.png'.format(i_frame))
+            i_frame += 1
     if save:
         os.system('apngasm /tmp/pvtol_pole_sim_1_anim.apng /tmp/sc/screenshot_0* 1 {:d}'.format(fps))
 
@@ -68,10 +82,10 @@ def main(_plot=True, _render=True):
     env_name = 'pvtol_pole-v0'
     env = gym.make(env_name)
     env.seed(123)
-    r = StateFeedbackReg(env)
+    r = StateFeedbackReg(env, 'lqr')
     time, X, U = sim(env, r)
     if _plot:
-        plot(time, X, U)
+        plot(time, X, U, filename='/tmp/pvtol_pole_sim.png')
         plt.show()
     if _render:
         render(env, time, X, U)
@@ -79,4 +93,4 @@ def main(_plot=True, _render=True):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     np.set_printoptions(linewidth=300, suppress=True)
-    main(_plot=True, _render=False)
+    main(_plot='plot' in sys.argv, _render= 'render' in sys.argv)
