2b91b027119b10915d41541d1c66622d6449b509
Poine <poinix@gmail.com>
2018-06-07 19:32:32 +0200

    training restart

--- src/gym-foo/gym_foo/__init__.py
+++ src/gym-foo/gym_foo/__init__.py
@@ -1,10 +1,27 @@
 from gym.envs.registration import register
 
+## Pendulum
+##
+# copy of original gym code
 register(
-    id='foo-v0',
-    entry_point='gym_foo.envs:FooEnv',
+    id='pendulum-legacy-v0',
+    entry_point='gym_foo.envs:PendulumLegacyEnv',
+)
+# modified to use angle as observation
+register(
+    id='pendulum-ang-obs-v0',
+    entry_point='gym_foo.envs:PendulumAngleObsEnv',
 )
 
+
+## CartPole
+##
+# modification of the original gym code for continuous input
+register(
+    id='cont-cartpole-v0',
+    entry_point='gym_foo.envs:FooEnv',
+)
+# added a position input driving a reference model
 register(
     id='cont-cartpole-with-ref-v0',
     entry_point='gym_foo.envs:CartPoleWithRefEnv',
